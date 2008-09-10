3b370bf36994a9ca5fb38782a303ca41de78aad8
aivosha <aivosha@b2cd42ac-7ed8-11dd-8b6f-79bb0735e950>
2008-09-10 04:26:06 +0000

    nada.

--- trunk/index.py
+++ trunk/index.py
@@ -1,23 +1,15 @@
 from time import time
-import hashlib
 
 from wsgiref import handlers
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
-
+from google.appengine.api import urlfetch
 
 class MainHandler(webapp.RequestHandler):
-  """Class for handling the root URL.
-  """
-
   def get(self):
-    ts = str(int(time()))
-    appid = "2bDHbf3IkY2bp01sBtFL9nO_0_Bkcl0-"
-    secret = "a34f389cbd135de4618eed5e23409d34450"
+    page = urlfetch.fetch('http://www.yellowpages.com/sitemap_index.xml')
+    self.response.out.write(page.content)
 
-    sig = hashlib.md5("/WSLogin/V1/wslogin?appid=" + appid + "&ts=" + ts + secret).hexdigest()
-
-    self.response.out.write(template.render('hello.html', {'ts': ts, 'sig': sig, 'appid': appid }))
 
 def main():
   application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
