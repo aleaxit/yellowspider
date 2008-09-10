9a21b404033a869f36ffce0f602a86c72975ec58
aivosha <aivosha@b2cd42ac-7ed8-11dd-8b6f-79bb0735e950>
2008-09-10 12:17:11 +0000

    it can be a local app, doesnt need to bee gae.

--- index.py
+++ index.py
@@ -1,20 +1,4 @@
-from time import time
+import urllib2
 
-from wsgiref import handlers
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template
-from google.appengine.api import urlfetch
-
-class MainHandler(webapp.RequestHandler):
-  def get(self):
-    page = urlfetch.fetch('http://www.yellowpages.com/sitemap_index.xml')
-    self.response.out.write(page.content)
-
-
-def main():
-  application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
-  handlers.CGIHandler().run(application)
-
-
-if __name__ == '__main__':
-  main()
+page = urllib2.urlopen('http://www.yellowpages.com/sitemap_index.xml')
+print page.read()
